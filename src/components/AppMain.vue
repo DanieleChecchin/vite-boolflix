554c89aca817630f66e13cea31799fffb19fe246
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-23 15:08:51 +0200

    lingua film convertita in bandiera

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,36 +7,18 @@ export default {
             store
         }
     },
-    computed: {
-        langFlag() {
-            switch (this.film.original_language) {
-                case 'en':
-                    return "./flags/flag_inghilterra.png";
-
-                case 'it':
-                    return "./flags/flag_italia.jpg";
-
-                case 'fr':
-                    return "./flags/flag_francia.png";
-
-                case 'pt':
-                    return "./flags/flag_portogallo.png";
-
-                case 'ja':
-                    return "./flags/flag_giappone.png";
-
-                case 'de':
-                    return "./flags/flag_germany.jpg";
-
-                case 'es':
-                    return "./flags/flag_spagna.png";
-
-                default:
-                    return "./flags/flag_sconosciuta.jpg"
+    methods: {
+        getFlagImage(lang) {
+            if (['de', 'en', 'es', 'fr', 'it', 'ja', 'pt'].includes(lang)) {
+                return `/src/assets/${lang}.png`
+            } else {
+                return '/src/assets/unknown.png'
             }
 
         }
     }
+
+
 }
 </script>
 
@@ -47,7 +29,7 @@ export default {
         <li v-for="film in store.filmList">
             <span>Titolo del film:</span> {{ film.title }} <br>
             <span>Titolo originiale del film:</span> {{ film.original_title }} <br>
-            <span>Lingua originale:</span> {{ film.original_language }} <br>
+            <span>Lingua originale:</span> <img :src="getFlagImage(film.original_language)" alt="flag"><br>
             <span>Voto:</span> {{ film.vote_average }}
         </li>
     </ul>
