527985f6b885f344b31c5305b848d8f8751f594f
DanieleChecchin <danielechecchin26@gmail.com>
2024-09-23 11:48:16 +0200

    aggiunte immagini

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,20 +6,55 @@ export default {
         return {
             store
         }
+    },
+    computed: {
+        langFlag() {
+            switch (this.film.original_language) {
+                case 'en':
+                    return "./flags/flag_inghilterra.png";
+
+                case 'it':
+                    return "./flags/flag_italia.jpg";
+
+                case 'fr':
+                    return "./flags/flag_francia.png";
+
+                case 'pt':
+                    return "./flags/flag_portogallo.png";
+
+                case 'ja':
+                    return "./flags/flag_giappone.png";
+
+                case 'de':
+                    return "./flags/flag_germany.jpg";
+
+                case 'es':
+                    return "./flags/flag_spagna.png";
+
+                default:
+                    return "./flags/flag_sconosciuta.jpg"
+            }
+
+        }
     }
 }
 </script>
 
 <template>
-    <h1>AppMain</h1>
+
+    <h1 class="mb-3">{{ store.inputField.toUpperCase() }}</h1>
     <ul>
         <li v-for="film in store.filmList">
-            Titolo del film: {{ film.title }} <br>
-            Titolo originiale del film: {{ film.original_title }} <br>
-            Lingua originale: {{ film.original_language }} <br>
-            Voto: {{ film.vote_average }}
+            <span>Titolo del film:</span> {{ film.title }} <br>
+            <span>Titolo originiale del film:</span> {{ film.original_title }} <br>
+            <span>Lingua originale:</span> {{ film.original_language }} <br>
+            <span>Voto:</span> {{ film.vote_average }}
         </li>
     </ul>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+span {
+    font-weight: bold;
+}
+</style>
